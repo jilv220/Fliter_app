30283ddefa4b98aebef970712e4baae668317f54
jilv220 <LyuJi220@hotmail.com>
2018-12-02 16:29:49 -0600

    adding the chosing button but not the pic gallery or taking photos

--- app/src/main/java/com/example/lyuji/fliter_app/MainActivity.java
+++ app/src/main/java/com/example/lyuji/fliter_app/MainActivity.java
@@ -1,15 +1,64 @@
 package com.example.lyuji.fliter_app;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addListenerOnButton();
+
     }
 
+    public void addListenerOnButton() {
 
+        image = (ImageView) findViewById(R.id.imageView);
+
+        button = (Button) findViewById(R.id.imageSubmit);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                image.setImageResource(R.drawable.ic_launcher_background);
+            }
+
+        });
+
+    }
 }
